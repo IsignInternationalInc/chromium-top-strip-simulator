30e0a13131bd5caa23f3b0d3df78f00433be3302
tishka25 <teodorstanishev@gmail.com>
2021-12-10 17:23:51 +0200

    changed icons

--- src/components/Strip/Strip.tsx
+++ src/components/Strip/Strip.tsx
@@ -15,7 +15,7 @@ export default function Strip() {
       <SearchBar />
       <div className="strip-navigation-container">
         <Icon src={require("../../icons/Snapshot 16px.svg").default} className="strip-icon"/>
-        <Icon src={require("../../icons/Reading list 16px.svg").default} className="strip-icon"/>
+        <Icon src={require("../../icons/Add to reading list 16 px.svg").default} className="strip-icon"/>
         <Icon src={require("../../icons/QR Code 16px.svg").default} className="strip-icon"/>
         <Icon src={require("../../icons/Customize icon 16px.svg").default} className="strip-icon"/>
         <Icon src={require("../../icons/Customize icon 16px_v.1.svg").default} className="strip-icon"/>
